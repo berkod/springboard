fa2952ba1f4c7554f6a86145d724016195e35f72
Danylo Berko <danylo_berko@condenast.com>
2016-06-15 18:56:07 -0400

    standardized name, split product code columns

--- exercise1/exercise1.r
+++ exercise1/exercise1.r
@@ -1,3 +1,19 @@
+setwd('/Users/dberko/Documents/springboard/springboard/exercise1')
 library('tidyr')
 library('dplyr')
-data = read.csv('~/Documents/springboard/springboard/exercise1/refine_original.csv')
+data = read.csv('refine_original.csv', header=TRUE)
+# 1. standardize company name
+data$company <- data$company  %>% tolower()
+
+for(i in 1:length(data$company)){
+  if(startsWith(data$company[i], 'a')) {
+    data$company[i]= 'akzo'
+  } else if (startsWith(data$company[i], 'u') ){
+    data$company[i]= 'unilever'
+  } else if (startsWith(data$company[i], 'p') | startsWith(data$company[i], 'f') ){
+    data$company[i]= 'philips'
+  }
+}
+
+# 2: Separate product code and number
+data <- tidyr::separate(data, 'Product.code...number', c('product_code', 'product_number'), sep='-')
